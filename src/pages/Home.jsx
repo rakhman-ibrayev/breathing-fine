36722022a7fe5eeb6e97502892fdc04b8909f857
RakhmanIbrayev <rakhmanxibrayev@gmail.com>
2022-12-13 00:38:21 +0600

    added the Home page with Earth component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import { lazy, Suspense } from 'react'
+const Earth = lazy(() => import('@/components/Earth'))
+
+const Home = () => {
+    return (
+        <main>
+            <section className="home-hero">
+                <Suspense>
+                    <Earth
+                        widthPercentage={100}
+                    />
+                </Suspense>
+            </section>
+        </main>
+    )
+}
+
+export default Home
