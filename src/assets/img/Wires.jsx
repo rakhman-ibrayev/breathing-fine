fb39bf5f426d3ea2485868dac9e604cdd083d20f
RakhmanIbrayev <rakhmanxibrayev@gmail.com>
2022-12-22 22:09:03 +0600

    removed unused image and added a new one for error page

---
+++ src/assets/img/Wires.jsx
@@ -0,0 +1,14 @@
+const Wires = () => {
+    return (
+        <svg viewBox="0 0 1920 190" fill="none" preserveAspectRatio="xMinYMin meet">
+            <path fill="#fff" fillOpacity=".87" d="M463 0h264v190H463z" /><path d="M727 52h65a8 8 0 0 1 8 8v3a8 8 0 0 1-8 8h-65V52zm0 67h65a8 8 0 0 1 8 8v3a8 8 0 0 1-8 8h-65v-19z" fill="#888" />
+            <g fill="#fff">
+                <path fillOpacity=".6" d="M0 89h463v13H0z" /><path fillOpacity=".87" d="M1193 0h264v190h-264z" />
+            </g>
+            <path d="M1193 52h34a8 8 0 0 1 8 8v3a8 8 0 0 1-8 8h-34V52zm0 67h34a8 8 0 0 1 8 8v3a8 8 0 0 1-8 8h-34v-19z" fill="#090909" /><path fill="#fff" fillOpacity=".6" d="M1457 89h463v13h-463z" />
+        </svg>
+    )
+}
+
+export default Wires
+
