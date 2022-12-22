2cc901ec01a29fb64e1164dd5ce05a3fc6e92656
RakhmanIbrayev <rakhmanxibrayev@gmail.com>
2022-12-22 22:14:07 +0600

    added accessibility attributes to all svg images

--- src/assets/img/Ridges.jsx
+++ src/assets/img/Ridges.jsx
@@ -1,6 +1,7 @@
 const Ridges = () => {
     return (
-        <svg viewBox="0 0 490 74" preserveAspectRatio="xMinYMin meet">
+        <svg role="img" viewBox="0 0 490 74" preserveAspectRatio="xMinYMin meet">
+            <title>Арт с градиентным фоном</title>
             <path fillRule="evenodd" d="M404.755 0h-25.512L490 16.727v-3.853L404.755 0zM490 3.571L466.354 0H490v3.571zm0 22.458L317.644 0h-25.513L490 29.882v-3.853zm0 13.156L230.533 0H205.02L490 43.038v-3.853zm0 13.156L143.422 0h-25.514L490 56.193V52.34zm0 13.156L56.312 0H30.797L490 69.349v-3.853zM459.2 74L0 4.651v3.853L433.687 74H459.2zm-87.111 0L0 17.807v3.853L346.576 74h25.513zm-87.112 0L0 30.962v3.853L259.465 74h25.512zm-87.111 0L0 44.118v3.853L172.354 74h25.512zm-87.11 0L0 57.273v3.853L85.242 74h25.514zm-87.111 0L0 70.429V74h23.645z" fill="url(#A)"/>
             <defs>
                 <linearGradient id="A" x1="0" y1="37" x2="490" y2="37" gradientUnits="userSpaceOnUse">
--- src/assets/img/Wires.jsx
+++ src/assets/img/Wires.jsx
@@ -1,6 +1,7 @@
 const Wires = () => {
     return (
-        <svg viewBox="0 0 1920 190" fill="none" preserveAspectRatio="xMinYMin meet">
+        <svg role="img" viewBox="0 0 1920 190" fill="none" preserveAspectRatio="xMinYMin meet">
+            <title>Разъединенный провод</title>
             <path fill="#fff" fillOpacity=".87" d="M463 0h264v190H463z" /><path d="M727 52h65a8 8 0 0 1 8 8v3a8 8 0 0 1-8 8h-65V52zm0 67h65a8 8 0 0 1 8 8v3a8 8 0 0 1-8 8h-65v-19z" fill="#888" />
             <g fill="#fff">
                 <path fillOpacity=".6" d="M0 89h463v13H0z" /><path fillOpacity=".87" d="M1193 0h264v190h-264z" />
