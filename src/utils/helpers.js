06d243ca149e9584f35a45cd97dc7ab84de245d7
RakhmanIbrayev <rakhmanxibrayev@gmail.com>
2022-12-23 21:50:28 +0600

    created a new helper function that is useful in finding a characterization of a current AQI level

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -7,3 +7,18 @@ export const getSearchParams = (paramsObj) => {
 
     return searchString.substring(0, searchString.length - 1)
 }
+
+export const getAqiVerdict = (aqiLevel) => {
+    let verdict = ''
+
+    if (aqiLevel <= 50)
+        verdict = 'хорошо'
+    else if (aqiLevel <= 100)
+        verdict = 'средне'
+    else if (aqiLevel <= 150)
+        verdict = 'умеренно'
+    else
+        verdict = 'опасно'
+
+    return verdict
+}
