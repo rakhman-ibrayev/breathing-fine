10338388679f0d231536697ed34445477ed7f19e
RakhmanIbrayev <rakhmanxibrayev@gmail.com>
2022-12-23 21:44:25 +0600

    added new routes for error states and routes mismatch and added map data fethcing in App component for further data visualization on the Earth component or other purposes

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,20 +1,33 @@
-import { Routes, Route } from 'react-router-dom'
-import { lazy, Suspense } from 'react'
+import { Routes, Route, useNavigate } from 'react-router-dom'
+import { lazy, Suspense, useEffect, useState } from 'react'
+import { getMapData } from '@/utils/aqiService.js'
 import Nav from '@/components/Nav/Nav'
 import './App.css'
 
 const Home = lazy(() => import('@/pages/Home/Home'))
 const Search = lazy(() => import('@/pages/Search/Search'))
+const Error = lazy(() => import('@/pages/Error/Error'))
 
 function App() {
+    const [mapData, setMapData] = useState([])
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        getMapData()
+            .then(res => res.json())
+            .then(resObj => setMapData(resObj.data))
+            .catch(() => navigate('/error'))
+    }, [])
 
     return (
         <>
             <Nav />
             <Suspense>
                 <Routes>
-                    <Route path="/" element={<Home />} />
+                    <Route path="/" element={<Home mapData={mapData} />} />
                     <Route path="/search" element={<Search />} />
+                    <Route path="/error" element={<Error/>} />
+                    <Route path="*" element={<Error/>} />
                 </Routes>
             </Suspense>
         </>
