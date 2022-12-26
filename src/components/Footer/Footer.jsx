651287920185efdaf135f43d1a76cb1f07182be2
RakhmanIbrayev <rakhmanxibrayev@gmail.com>
2022-12-26 23:13:41 +0600

    made some minor style changes to the Footer component and added some accessibility tags

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,14 +5,15 @@ import './Footer.css'
 const Footer = () => {
     return (
         <footer>
-            <div className="container">
+            <div className="container flex column align-center">
                 <nav className="footer-nav">
                     <NavLinks block="footer-nav" />
                 </nav>
                 <a
                     href="https://github.com/rakhman-ibrayev/breathing-fine"
                     target="_blank" rel="noreferrer"
-                    className="github-link flex align-center justify-center"
+                    className="github-link flex align-center"
+                    aria-label="ссылка на репозиторий проекта в GitHub"
                 >
                     by Rakhman Ibrayev
                     <GitIcon />
