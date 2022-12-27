e97744df803a45b741b7f80a16149dbe9927ad7b
RakhmanIbrayev <rakhmanxibrayev@gmail.com>
2022-12-27 16:56:42 +0600

    changed a div to a semantic element in Intro component

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -3,10 +3,10 @@ import './Intro.css'
 const Intro = ({ smallText, bigText, infoSign }) => {
     return (
         <div className="intro flex justify-between">
-            <div className="intro__text flex column justify-between">
+            <hgroup className="intro__text flex column justify-between">
                 <p className="intro__small-text">{smallText}</p>
                 <h2 className="intro__big-text">{bigText}</h2>
-            </div>
+            </hgroup>
             <div aria-hidden="true" className="intro__info-sign flex justify-center align-center">
                 {infoSign}
             </div>
