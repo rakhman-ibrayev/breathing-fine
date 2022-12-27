b2a86bf4e848421f0e3fff8a840b9221a3f29b15
RakhmanIbrayev <rakhmanxibrayev@gmail.com>
2022-12-27 21:59:02 +0600

    Created the first information section component called HomeInfoPollution for the home page and moved all the section components from the home page to a directory closer to the Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
-import HomeHero from '@/components/HomeHero/HomeHero'
+import HomeHero from './sections/HomeHero/HomeHero'
+import HomeInfoPollution from './sections/HomeInfoPollution/HomeInfoPollution'
 import './Home.css'
 
 const Home = ({ mapData }) => {
     return (
-        <main>
+        <main className="home-page">
             <HomeHero mapData={mapData} />
+            <HomeInfoPollution />
         </main>
     )
 }
